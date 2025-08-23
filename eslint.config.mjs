56e94c0820a4da928170e99feee4d30454c12bed
tunacook <tunacook0704@gmail.com>
2025-08-23 17:21:32 +0900

    Add lint settings

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"]
+  },
+  {
+    languageOptions: { globals: globals.browser }
+  },
+  {
+    ignores: [
+        "dist/*.js",
+        "jest.config.js"
+    ]
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
