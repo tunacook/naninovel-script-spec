f062e5002aaa0ebcd2e5c1afd8fa86dff23ffb6d
tunacook <tunacook0704@gmail.com>
2026-01-10 15:19:41 +0900

    *ではなく明示的にexportする

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export * from "./naninovel";
+import {
+    isSkipNaninovelSyntax,
+    trimRuby,
+    trimBrTag,
+    trimFgTag,
+    isExtNani,
+    trimAuthor,
+    trimBracket,
+    trimSquareBrackets
+} from "./naninovel";
+
+export {
+    isSkipNaninovelSyntax,
+    trimRuby,
+    trimBrTag,
+    trimFgTag,
+    isExtNani,
+    trimAuthor,
+    trimBracket,
+    trimSquareBrackets
+};
